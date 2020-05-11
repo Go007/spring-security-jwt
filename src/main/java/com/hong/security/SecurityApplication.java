29f6bee9df37bb93bcaeac81b3e19ec81259d3ca
hong <1536054473@qq.com>
2020-05-11 21:26:12 +0800

    init

---
+++ src/main/java/com/hong/security/SecurityApplication.java
@@ -0,0 +1,15 @@
+package com.hong.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wanghong
+ * @date 2020/05/11 21:19
+ **/
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
